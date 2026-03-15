186abc046cea2307d59f5b87e8adfb825a55ee89
Jakub Dąbrowski <60621737+tuniemanicku@users.noreply.github.com>
2026-03-15 14:04:54 +0100

    Added requirements

--- data_downloader.py
+++ data_downloader.py
@@ -1,8 +1,19 @@
 from pinterest_dl import PinterestDL
 
-# Search for images
-images = PinterestDL.with_api().search_and_download(
-    query="memes",
-    output_dir="test_memes",
-    num=30
-)
+def main():
+
+    # Search for train images
+    images = PinterestDL.with_api().search_and_download(
+        query="memes",
+        output_dir="memes",
+        num=150
+    )
+    # Search for test images
+    images = PinterestDL.with_api().search_and_download(
+        query="funny_memes",
+        output_dir="test_memes",
+        num=30
+    )
+
+if __name__ == "__main___":
+    main()
